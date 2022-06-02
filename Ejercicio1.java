4b89842fee7ba9417a1b8f64578a4e6f9bd99a0a
waltermilcoff <waltermilcoff@gmail.com>
2022-06-02 16:19:11 -0300

    Ejercicio 1

---
+++ Ejercicio1.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        List<String> palabras = new ArrayList<>();
+        palabras.add("Hola");
+        palabras.add(null);
+        palabras.add("Informatorio");
+        palabras.add("");
+        List<String> valoresFiltrados = palabras.stream()
+                .filter(Objects::nonNull)
+                .filter(x -> !x.equals(""))
+                .collect(Collectors.toList());
+        System.out.println(valoresFiltrados);
+    }
+
+}
